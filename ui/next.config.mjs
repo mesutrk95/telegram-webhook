8cfa25f387d2584a1e805e31642410e0f49d5f7d
Masoud Rezakhanlou <mrezakhanlou@ecotrak.com>
2024-04-07 21:46:58 +0400

    env update

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({path: '../.env'})
+dotenv.config({ path: "../.env" });
 
 const nextConfig = {
   images: { unoptimized: true },
-  output: "export", 
-  ...(process.env.EXPORT_UI_DIR && { distDir: "../dist" }),
-
+  ...(process.env.UI_EXPORT_DIR && {
+    distDir: process.env.UI_EXPORT_DIR,
+    output: "export",
+  }),
+  ...(process.env.UI_BASE_URL && { baseUrl: process.env.UI_BASE_URL }),
 };
 
 export default nextConfig;
