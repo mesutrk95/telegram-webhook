a5e5ea195d7f2f446c24a827137fe48a9a9cbeaa
Masoud Rezakhanlou <masoud.rk95@gmail.com>
2024-04-24 21:21:21 +0400

    cleanup

--- web/src/index.js
+++ web/src/index.js
@@ -12,7 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
   <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
-    safa
     <Routes>
       <Route path="/signin" element={<SignIn />} />
       <Route path="/setup" element={<Setup />} />
